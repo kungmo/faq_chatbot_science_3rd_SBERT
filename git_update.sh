a3eb30ecf9646312c7d65a6899dedb1507e0025e
kungmo <kungmo@gmail.com>
2022-08-30 22:42:09 +0900

    gunicorn과 nginx timeout 문제 해결하고 배포

--- git_update.sh
+++ git_update.sh
@@ -3,5 +3,5 @@
 echo "git 업데이트"
 
 git add .
-git commit -m "git 동기화용 commit"
+git commit -m "gunicorn과 nginx timeout 문제 해결하고 배포"
 git push origin main
--- run_background.sh
+++ run_background.sh
@@ -1,1 +1,2 @@
+export PYTHONIOENCODING=utf-8
 nohup python3 manage.py runserver 0.0.0.0:8000 &
